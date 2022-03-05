f43494b15f9609b431bd32e372a9465347ea6e5b
italomanzine <italomanzine@gmail.com>
2022-03-04 22:19:56 -0300

    start led and buzzer

---
+++ software/timingSystemSoft.X/main.c
@@ -0,0 +1,72 @@
+/*
+ * UFSC- Universidade Federal de Santa Catarina
+ * Projeto: Trabalho Processo Industrial de programação C para PIC
+ * File:   main.c
+ * Author: Ítalo Manzine e Yan Bentes
+ * Sistema embarcado para um sistema de saída e cronometragem em competições de natação
+ * 
+ * Created on 04 de Março de 2022, 19:02
+ */
+
+// PIC16F877A Configuration Bit Settings
+// CONFIG
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#include <xc.h> //include padrão do compilador XC8
+#include <pic16f877a.h> //include padrão do PIC16F877A
+#include <stdio.h> //include padrão de entradas e saídas da linguagem C
+
+#define _XTAL_FREQ 4000000 //Define a frequencia do clock em 4MHz
+
+#define START RB0
+#define LIGHT RC0
+#define BUZZER RC2
+
+void buzzer(void) 
+{
+    BUZZER = 1;
+    __delay_ms(200);
+    BUZZER = 0;
+}
+
+void light(void) 
+{
+    LIGHT = 1;
+    __delay_ms(150);
+    LIGHT = 0;
+}
+    
+
+
+void main(void) {
+    //inicialização das portas todas em saída
+    TRISB = 0xFF;
+    TRISC = 0x00;
+    
+    // Resistores Pull-up ativados
+    OPTION_REG = 0b01111111;
+    
+    LIGHT = 0;
+    BUZZER = 0;
+    
+    while (1) // LOOP PRINCIPAL
+    {
+        if (START == 0) 
+        {
+            LIGHT = 1;
+            BUZZER = 1;
+            __delay_ms(200);
+            LIGHT = 0;
+            BUZZER = 0;
+        }
+    }
+    
+    return;
+}
